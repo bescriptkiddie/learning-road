0fb93c7e526285fa144eadbc8165bf2798f1b312
pika <1040906259@qq.com>
2020-07-06 02:51:46 +0800

    0704 lesson1

--- 作业/0704 + 邱源茂+ 数据劫持/code/myvue.js
+++ 作业/0704 + 邱源茂+ 数据劫持/code/myvue.js
@@ -1,78 +1,77 @@
-class Vue extends EventTarget{
-    constructor(options){
-        super();
-        this.$options = options;
-        this._data = options.data;
-        this.observer(this._data);
-        this.compile();
+class Vue extends EventTarget {
+  constructor(options) {
+    super()
+    this.$options = options
+    this._data = options.data
+    this.observer(this._data)
+    this.compile()
+  }
+  observer(data) {
+    for (let key in data) {
+      let value = data[key]
+      let _this = this
+      Object.defineProperty(data, key, {
+        configurable: true,
+        enumerable: true,
+        get() {
+          console.log('get...')
+          return value
+        },
+        set(newValue) {
+          console.log('set...')
+          let event = new CustomEvent(key, {
+            detail: newValue,
+          })
+          _this.dispatchEvent(event)
+          value = newValue
+        },
+      })
     }
-    observer(data){
-        for(let key in data){
-            let value = data[key];
-            let _this = this;
-            Object.defineProperty(data,key,{
-                configurable:true,
-                enumerable:true,
-                get(){
-                   console.log("get..."); 
-                   return value;
-                },
-                set(newValue){
-                    console.log("set...");
-                    let event = new CustomEvent(key,{
-                       detail: newValue
-                    });
-                    _this.dispatchEvent(event)
-                    value = newValue;
-                }
-            })
+  }
+
+  compile() {
+    let ele = document.querySelector(this.$options.el)
+    this.compileNodes(ele)
+  }
+  compileNodes(ele) {
+    let childNodes = ele.childNodes
+    // console.log(childNodes);
+    childNodes.forEach((node) => {
+      // console.log(node);
+      // 文本  元素节点;
+      if (node.nodeType === 1) {
+        //    元素节点
+        if (node.childNodes.length > 0) {
+          this.compileNodes(node)
         }
-
-    }
-
-    compile(){
-        let ele = document.querySelector(this.$options.el);
-        this.compileNodes(ele);
-    }
-    compileNodes(ele){
-        let childNodes = ele.childNodes;
-        // console.log(childNodes);
-        childNodes.forEach(node=>{
-            // console.log(node);
-            // 文本  元素节点;
-           if( node.nodeType ===1){
-            //    元素节点
-            if(node.childNodes.length>0){
-                this.compileNodes(node);
-            }
-           }else if(node.nodeType===3){
-            //    文本
-            // 匹配 ”{{}}“的文本
-            let reg = /\{\{\s*([^\{\}\s]+)\s*\}\}/g;
-            let textContent = node.textContent;
-            console.log("??",textContent);
-            if(reg.test(textContent)){
-                console.log("有大括号表达式");
-                // 获取数据的下标
-                // RegExp  Function  Object
-                let $1 = RegExp.$1;
-                // RegExp.$2
-                // console.log($1);
-               let rData =  this._data[$1];
-               console.log(rData);
-            //    console.log(this.$options.data);
-                //    将数据渲染到视图；
-                node.textContent = node.textContent.replace(reg,rData);
-                this.addEventListener($1,e=>{
-                    console.log("自定义事件",e.detail);
-                    // 重新渲染模板；获取新值；
-                    let oldValue = this._data[$1];
-                    let newValue = e.detail;
-                    let reg = new RegExp(oldValue);
-                    node.textContent = node.textContent.replace(reg,newValue);
-                })
-            }
-           }
-        })
-    }
+      } else if (node.nodeType === 3) {
+        //    文本
+        // 匹配 ”{{}}“的文本
+        let reg = /\{\{\s*([^\{\}\s]+)\s*\}\}/g
+        let textContent = node.textContent
+        console.log('??', textContent)
+        if (reg.test(textContent)) {
+          console.log('有大括号表达式')
+          // 获取数据的下标
+          // RegExp  Function  Object
+          let $1 = RegExp.$1
+          // RegExp.$2
+          // console.log($1);
+          let rData = this._data[$1]
+          console.log(rData)
+          //    console.log(this.$options.data);
+          //    将数据渲染到视图；
+          node.textContent = node.textContent.replace(reg, rData)
+          this.addEventListener($1, (e) => {
+            console.log('自定义事件', e.detail)
+            // 重新渲染模板；获取新值；
+            let oldValue = this._data[$1]
+            let newValue = e.detail
+            let reg = new RegExp(oldValue)
+            node.textContent = node.textContent.replace(reg, newValue)
+          })
+        }
+      }
+    })
+  }
 }
--- 作业/0704 + 邱源茂+ 数据劫持/coding/myvue.js
+++ 作业/0704 + 邱源茂+ 数据劫持/coding/myvue.js
@@ -1,42 +1,40 @@
 class Vue {
-    constructor(options) {
-      this.$options = options
-      this.data = options.data
-      this.observe = 
+  constructor(options) {
+    this.$options = options
     this.complie()
   }
   complie() {
     let ele = document.querySelector(this.$options.el)
-    console.log()
-    this.complieNodes()
+    this.complieNodes(ele)
   }
-  complieNodes() {
+  complieNodes(ele) {
     let childNodes = ele.childNodes
+    console.log(childNodes)
     childNodes.forEach((node) => {
       console.log(node)
       if (node.nodeType === 1) {
-        //元素节点
+        /* 元素节点
+          递归调用compliwNodes  -->巧妙判断,就是利用length>0,一层层找下去*/
+        if (node.childNodes.length > 0) {
+          this.complieNodes(node)
+        }
       } else if (node.nodeType === 3) {
-        //
-        let reg = '/{{s*([^{}s]+)s*}}/'
+        /* 文本节点 ---> 匹配"{{}}"
+         *:0-n ,+:1-n s:空格 S:非空格  []:集合 ^:非 */
+        let reg = /\{\{\s*([^\{\}\s]+)\s*\}\}/g
         let textContent = node.textContent
-        console.log('?', textContent)
         if (reg.test(textContent)) {
-          console.log('有大括号的表达式')
-          //获取数据下标
+          console.log('有大括号表达式!')
+          // 获取下标分组  -->()里面的
           let $1 = RegExp.$1
-          //
+          // console.log($1)
           let rData = this.$options.data[$1]
-          console.log(rData)
-          //将数据渲染到视图
+          // console.log(rData)
+          console.log(textContent)
+          //将数据渲染到视图  reg 还可以作为参数在replace内用
           node.textContent = node.textContent.replace(reg, rData)
         }
       }
     })
   }
-    observe(data) {
-        for (let key in data)
-            let value = data[key]
-        
-    }
 }
