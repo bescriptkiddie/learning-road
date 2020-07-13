3cfb262ff930732af952d470a027b697ea2e4b64
pika <1040906259@qq.com>
2020-07-14 02:04:06 +0800

    0711 finished

--- 直播课课件/07-11 node 01/code/commandjs-teach/index.js
+++ 直播课课件/07-11 node 01/code/commandjs-teach/index.js
@@ -1,6 +1,6 @@
 // add 移动到一个单独的文件
 // 导入
-// ./  / 
+// ./  /
 // 补齐
 // .js || .json || .node
 // const add = require('./add');
@@ -9,15 +9,15 @@
 
 // 如果不是文件的话
 // 尝试看看是不是文件夹
-// 1. package.json  里面的 main 字段 
+// 1. package.json  里面的 main 字段
 // 2. 找里面的 index.js ||.json || .node
 // const sub = require('./sub');
 // console.log(sub)
 
 // 如果不是 ./ / 开头的话
 // 加载内置模块 第三方模块
-// const http = require('http');
-// console.log(http)
+const http = require("http")    
+console.log(http)
 
 // 文件夹以及文件同名
 // const add = require('./add');
@@ -26,5 +26,5 @@
 // 第三方模块
 // const atest = require('atest');
 // console.log(atest)
-const lodash = require('lodash');
-console.log(lodash)
+// const lodash = require('lodash');
+// console.log(lodash)
