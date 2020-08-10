e796278f4fc8b39dfd1cb732bf7aadb30d97c9c3
pika <1040906259@qq.com>
2020-08-10 09:10:01 +0800

    vue第一节

--- 直播课课件/08-06 typescript 02/代码/ts-webserver/src/controllers/user.ts
+++ 直播课课件/08-06 typescript 02/代码/ts-webserver/src/controllers/user.ts
@@ -1,11 +1,12 @@
-import { Post} from '../kkb/index';
+import { Post } from '../kkb/index'
 
 export default class UserController {
-
-    @Post('/login')
-    public login(ctx) {
-        console.log('login');
-        ctx.body = '登陆';
+  @Post('/login')
+  public login(ctx) {
+    console.log('login')
+    ctx.body = {
+      status: 200,
+      msg: '通过postman进来了',
     }
-
+  }
 }
--- 直播课课件/08-06 typescript 02/代码/ts-webserver/src/kkb/index.ts
+++ 直播课课件/08-06 typescript 02/代码/ts-webserver/src/kkb/index.ts
@@ -1,13 +1,13 @@
-import KoaRouter from 'koa-router';
-import glob from 'glob';
-import MainController from '../controllers/main';
-
+import KoaRouter from 'koa-router'
+import glob from 'glob'
+import MainController from '../controllers/main'
+import UserController from '../controllers/user'
 
 interface KKBOptions {
-    // 设置控制器文件存在的目录
-    controllersPath: string;
-    // 路由对象
-    router: KoaRouter;
+  // 设置控制器文件存在的目录
+  controllersPath: string
+  // 路由对象
+  router: KoaRouter
 }
 
 // let arr = {
@@ -24,84 +24,88 @@ interface KKBOptions {
 // }
 
 export default class KKB {
+  // private options: KKBOptions;
 
-    // private options: KKBOptions;
+  constructor(private options: KKBOptions) {
+    // this.options = options;
 
-    constructor(private options: KKBOptions) {
-        // this.options = options;
+    // 根据options.controllersPath加载所有的controller文件
+    this.loadControllers()
+  }
 
-        // 根据options.controllersPath加载所有的controller文件
-        this.loadControllers();
-    }
+  private loadControllers() {
+    let controllerFiles = glob.sync(this.options.controllersPath)
 
-    private loadControllers() {
+    console.log('controllerFiles', controllerFiles)
 
-        let controllerFiles = glob.sync(this.options.controllersPath);
+    // 循环所有的controller文件，并加载，进行实例化，然后把实例化以后的方法与对应的路由（url）进行绑定
+    controllerFiles.forEach((controllerFile) => {
+      // console.log('controllerFile', controllerFile);
+      // let Controler = await import(controllerFile);
 
-        console.log('controllerFiles', controllerFiles);
+      let Controller = require(controllerFile).default
 
-        // 循环所有的controller文件，并加载，进行实例化，然后把实例化以后的方法与对应的路由（url）进行绑定
-        controllerFiles.forEach( controllerFile => {
-            // console.log('controllerFile', controllerFile);
-            // let Controler = await import(controllerFile);
+      // console.log('Controller', Controller);
 
-            let Controller = require(controllerFile).default;
+      let controller = new Controller()
 
-            // console.log('Controller', Controller);
+      // controller.__controllers = [
+      //     {
+      //         verb: 'get',
+      //         url: '/',
+      //         name: 'index'
+      //     },
+      //     {
+      //         verb: 'post',
+      //         url: '/login',
+      //         name: 'login'
+      //     }
+      // ];
 
-            let controller = new Controller();
+      // controller.index;
 
-            // controller.__controllers = [
-            //     {
-            //         verb: 'get',
-            //         url: '/',
-            //         name: 'index'
-            //     },
-            //     {
-            //         verb: 'post',
-            //         url: '/login',
-            //         name: 'login'
-            //     }
-            // ];
+      // if ( controller.index ) {
+      //     this.options.router.get('/', controller.index);
+      // }
 
-            // controller.index;
+      // 当前的控制器对象的哪些方法与哪些url进行绑定
+      // console.log('controller', controller.__controllers);
 
-            // if ( controller.index ) {
-            //     this.options.router.get('/', controller.index);
-            // }
-
-            // 当前的控制器对象的哪些方法与哪些url进行绑定
-            // console.log('controller', controller.__controllers);
-
-            if (controller.__controllers) {
-                controller.__controllers.forEach( __controller => {
-                    this.options.router[__controller.verb](
-                        __controller.url, 
-                        controller[__controller.name]
-                    );
-                } );
-            }
-            
-        } );
-
-    }
-
+      if (controller.__controllers) {
+        controller.__controllers.forEach((__controller) => {
+          this.options.router[__controller.verb](
+            __controller.url,
+            controller[__controller.name],
+          )
+        })
+      }
+    })
+  }
 }
 
 export function Get(url: string) {
-    return function(target: any, name: string, descriptor: PropertyDescriptor) {
-        // __controllers 存储的就是当前 target 中的所有 方法 与 对应的url
-        if (!target.__controllers) {
-            target.__controllers = [];
-        }
-        target.__controllers.push({
-            verb: 'get',
-            url,
-            name
-        });
+  return function (target: any, name: string, descriptor: PropertyDescriptor) {
+    // __controllers 存储的就是当前 target 中的所有 方法 与 对应的url
+    if (!target.__controllers) {
+      target.__controllers = []
     }
+    target.__controllers.push({
+      verb: 'get',
+      url,
+      name,
+    })
+  }
 }
 
-export function Post() {
-    // 你们的代码就写在这里面，记得这里的代码要截图 - 赠小乐！
+export function Post(url: string) {
+  return function (target: any, name: string, descriptor: PropertyDescriptor) {
+    if (!target.__controllers) {
+      target.__controllers = []
+    }
+    target.__controllers.push({
+      verb: 'post',
+      url,
+      name,
+    })
+  }
 }
